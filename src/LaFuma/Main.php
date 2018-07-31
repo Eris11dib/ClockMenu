ebed7d60ec4f2dc47c2cd67fab63d961a6bb8f46
Eris11dib <datfumatore@gmail.com>
2018-07-31 18:30:29 +0200

    Update Main.php

--- src/LaFuma/Main.php
+++ src/LaFuma/Main.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace LaFuma;
+namespace ClockMenu;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat as Color;
@@ -15,10 +15,23 @@ use pocketmine\event\player\PlayerInteractEvent;
 
 class Main extends PluginBase implements Listener{
 	
+	public $config;
+	
 	public function onEnable(){
 		
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getLogger()->info("\n§l§cPlugin attivato con successo!\nSeguimi su Telegram @LaFuma per news");
+		$this->getLogger()->info("§6Activated");
+		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML,[
+		"clock_name" => "Your Server Name",
+		"first_item_name" => "Your Things",
+		"second_item_name" => "Your things",
+		"third_item_name" => "Your things",
+		"fourth_item_name" => "Your Things",
+		
+		"second_item_teleport" => "your world name",
+		"third_item_teleport" => "your world"
+		
+		]);
 		}
 		
 		public function onJoin(PlayerJoinEvent $event){
@@ -26,7 +39,7 @@ class Main extends PluginBase implements Listener{
 			$player = $event->getPlayer();
 			$player->getInventory()->clearAll();
 			$item1 = Item::get(Item::CLOCK);
-			$item1->setCustomName("§l§bSolarDinasty§r");
+			$item1->setCustomName($this->config->get("clock_name"));
 			$player->getInventory()->setItem(4, $item1);
 		
 	}
@@ -35,61 +48,39 @@ class Main extends PluginBase implements Listener{
 		
 		$player = $event1->getPlayer();
 		$item = $player->getInventory()->getItemInHand();
-		if($item->getCustomName() == "§l§bSolarDinasty§r"){
+		if($item->getCustomName() == this->config->get("clock_name")){
 			$player->getInventory()->clearAll();
-			//Info
+			//Things 1 
 			$item2 = Item::get(Item::SLIMEBALL, 12,1);
-			$item2->setCustomName("§l§6Info§r");
+			$item2->setCustomName($this->config->get("first_item_name"));
 			$player->getInventory()->setItem(4,$item2);
-			//KitPvP
+			//Thinga 2
 			$item3 = Item::get(Item::DIAMOND_SWORD, 11,1);
-			$item3->setCustomName("§l§6KitPvP§r");
+			$item3->setCustomName($this->config->get("second_item_name"));
 			$player->getInventory()->setItem(5,$item3);
-			//Prison
+			//Things 3 
 			$item4 = Item::get(Item::DIAMOND_PICKAXE, 10,1);
-			$item4->setCustomName("§l§6Prison§r");
+			$item4->setCustomName($this->config->get("third_item_name"));
 			$player->getInventory()->setItem(3,$item4);
-			//Back 1
+			//Things 4
 			$item1 = Item::get(Item::STICK, 14,1);
-			$item1->setCustomName("§4§lBack§r");
+			$item1->setCustomName($this->config->get("fourth_item_name"));
 			$player->getInventory()->setItem(8,$item1);
-			//Back 2
+			//Things 5
 			$item5 = Item::get(Item::STICK,9,1);
-			$item5->setCustomName("§l§4Back§r");
+			$item5->setCustomName($this->config->get("fourth_item_name"));
 			$player->getInventory()->setItem(0,$item5);
-			//Ignore This
-			//$item6 = Item::get(Item::WOOL,8,1);
-			//$item6->setCustomName("§l§cIgnore this§r");
-			//$player->getInventory()->setItem(2,$item6);
-			//Ignore This2
-			//$item7 = Item::get(Item::WOOL,7,1);
-			//$item7->setCustomName("§l§cIgnore this§r");
-			//$player->getInventory()->setItem(1,$item7);
-			//Ignore This3
-			//$item8 = Item::get(Item::WOOL,6,1);
-			//$item8->setCustomName("§l§cIgnore this§r");
-			//$player->getInventory()->setItem(6,$item8);
-			//Ignore This4
-			//$item9 = Item::get(Item::WOOL,5,1);
-			//$item9->setCustomName("§l§cIgnore this§r");
-			//$player->getInventory()->setItem(7,$item9);
 			
-}else if($item->getCustomName() == "§4§lBack§r"){
+}else if($item->getCustomName() == $this->config->get("fourth_item_name")){
 	$player->getInventory()->clearAll();
 	$item1 = Item::get(Item::CLOCK);
-    $item1->setCustomName("§l§bSolarDinasty§r");
+    $item1->setCustomName($this->config->get("clock_name"));
 	$player->getInventory()->setItem(4, $item1);
 	
-  }else if($item->getCustomName() == "§l§4Back§r"){
-  	$player->getInventory()->clearAll();
-      $item1 = Item::get(Item::CLOCK);
-      $item1->setCustomName("§l§bSolarDinasty§r");
-      $player->getInventory()->setItem(4,$item1);
-      
-}else if($item->getCustomName() == "§l§6KitPvP§r"){
+}else if($item->getCustomName() == $this->config->get("second_item_name")){
 $player->teleport($this->getServer()->getLevelByName("KitPvP")->getSafeSpawn());
 
-}else if($item->getCustomName() == "§l§6Prison§r"){
+}else if($item->getCustomName() == $this->config->get("third_item_name")){
 $player->teleport($this->getServer()->getLevelByName("Prison")->getSafeSpawn());
         }
     }
